7d42044b54ca5ee9c44fa72d668ec4a7b9e201f1
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-06-24 16:41:03 +0200

    #16 Create a Features Section
    Add a responsive features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { FeaturesSection } from "./ui/features-section";
 import { HeroSection } from "./ui/hero-section";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <FeaturesSection />
     </main>
   );
 }
